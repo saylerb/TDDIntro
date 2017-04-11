c3949d126722aaf96f6d48e6436370c578b93195
Brian Sayler <sayler.b@gmail.com>
2017-04-11 11:18:21 -0500

    implement first test
    - added constructor to Account class to initialize account balance
    - added deposit method to the Account class
    - using assertEquals was 'ambiguous' so I needed to cast it to Integer
      type explicitly

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    private Integer balance;
+
+    public Account(Integer balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(Integer amount) {
+        this.balance +=  amount;
+    }
+
+    public Integer getBalance() {
+        return this.balance;
+    }
 }
