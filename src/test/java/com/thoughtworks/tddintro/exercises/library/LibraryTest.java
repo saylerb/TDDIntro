77a7831f050b5a2d6e0bd93038588f81819b5480
Brian Sayler <sayler.b@gmail.com>
2017-04-11 14:17:36 -0500

    add when/then for removing book

--- src/test/java/com/thoughtworks/tddintro/exercises/library/LibraryTest.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/library/LibraryTest.java
@@ -68,9 +68,8 @@ public class LibraryTest {
     }
 
     @Test
-    @Ignore // Remove each @Ignore and implement test
     public void shouldDeleteBookFromCollectionWhenRemovedByUser() throws IOException {
-        // Add when/thenReturn here
+        when(bufferedReader.readLine()).thenReturn("The Two Towers");
 
         books.add("The Two Towers");
         library.removeBook();
