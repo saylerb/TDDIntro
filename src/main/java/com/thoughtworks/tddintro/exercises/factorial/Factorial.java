510ea80462ed59d8a655ac5cdb24d653468ef69a
Brian Sayler <sayler.b@gmail.com>
2017-04-10 22:04:49 -0500

    throw illegal arugment exception

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,6 +2,9 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        if (i < 0) {
+            throw new IllegalArgumentException();
+        }
         if (i == 0) {
             return 1;
         } else {
