7c5122ad9b5d3036f31e48ce1cbcadd221fc5fea
Brian Sayler <sayler.b@gmail.com>
2017-04-10 22:06:06 -0500

    refactor - combine into same if/then block

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -4,8 +4,7 @@ public class Factorial {
     public Integer compute(int i) {
         if (i < 0) {
             throw new IllegalArgumentException();
-        }
-        if (i == 0) {
+        } else if (i == 0) {
             return 1;
         } else {
             return i * compute(i - 1);
