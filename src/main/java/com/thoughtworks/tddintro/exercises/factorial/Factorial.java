c040334ceaf9f95ff1a6f66d8b06d73594119006
Brian Sayler <sayler.b@gmail.com>
2017-04-10 21:58:58 -0500

    make test pass with recursion

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,6 +2,10 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        return 1;
+        if (i == 1) {
+            return 1;
+        } else {
+            return i * compute(i - 1);
+        }
     }
 }
